2c2cd4d6e2e5a41d6312e9cebd26d106356477b1
SolalVall <solal.vallee@gmail.com>
2020-01-12 23:38:09 -0500

    [UPDATE] Add options and way to install package

--- README.md
+++ README.md
@@ -16,8 +16,8 @@ Basically we could say that's it is act like my own and custom cli package manag
 In order to install it, just open a terminal and copy/paste the following commands:
 
 ```
-git clone https://github.com/SolalVall/lazyTools.git /tmp
-sudo cp /tmp/lazyTools.sh /usr/local/bin/lt
+git clone https://github.com/SolalVall/lazyTools.git /tmp/lt
+sudo cp /tmp/lt/lazyTools.sh /usr/local/bin/lt
 ```
 
 To verify that lazyTools (```lt```) was correctly setup execute:
@@ -33,8 +33,8 @@ lt --version
   Example:
 
   ```
-  lt --git
-  lt --vagrant
+  lt --install git
+  lt -i vagrant
   ```
 
 - Check script version: ``` lt --version```
@@ -44,8 +44,8 @@ lt --version
 Please find below the historic of the different lazyTools releases:
  
 v0.0.1:
-  - Assign the correct package manager.
-  - Display script version. 
+  - Func for assigning the correct package manager.
   - Default Functions for verifying a package and install it.
   - Readme.
+  - Options added: --version, --help, --list
   - Tools added: git, vagrant, virtualbox
--- lazyTools.sh
+++ lazyTools.sh
@@ -1,60 +1,105 @@
-#!/bin/sh
+#!/bin/bash
 
 # GENERIC VARS
 LT_VERSION="0.0.1"
 PACKAGE_MANAGER=""
 PACKAGE_NAME=""
+PACKAGE_LIST=("git" "vagrant" "virtualbox")
+DISPLAY_HELP="$(basename "$0") [-h|--help] [-l|--list] [-i|--install] [-v|--version]
+
+Description: 
+
+	lt (lazyTools) allows you to automatically install some packages by using your package
+	manager or via some extra-steps and configuration
+
+Options:
+	
+	[-h|--help] Display the following message 
+	[-l|--list] List all available packages
+	[-i|--install] Install a specific package
+	[-v|--version] Display current version of the script
+
+Examples:
+	
+	lt --install package
+"
 
 #GENERIC FUNCS
 verify_package() {
-	#Remove -- from user input
-	PACKAGE_NAME=${1#--}
+	PACKAGE_NAME=$1
 
 	#Verify if package is already or not (Depends of which command results status)
 	IS_PACKAGE_INSTALLED=$(which $PACKAGE_NAME)
 	if [ $? -ne 0 ]; then
 		echo "$PACKAGE_NAME not installed"
-		if [ "$2" = "default_install" ]; then
+		if [[ "$2" = "default_install" ]]; then
 			echo "Start installation via $PACKAGE_MANAGER ..."
 			install_package $PACKAGE_NAME
 		fi
 	else
 		echo "$PACKAGE_NAME already installed"
-		break
 	fi
 }
 
 install_package() {
 	sudo $PACKAGE_MANAGER update
 	sudo $PACKAGE_MANAGER install $1 -y
-	echo "Package $PACKAGE_NAME installed"
+	echo "Package $1 installed"
 }
 
-## In case user want to know the version of the script
-if [ $1 = "--version" ]; then 
-	echo "v$LT_VERSION"
-	exit 0
-fi
- 
-## Set user distrib
-echo "Verify Linux OS Distribution (Script only compatible with Debian or Redhat)"
 
-if [ -f /etc/lsb-release ]; then
-	echo "Debian detetcted"
-	PACKAGE_MANAGER="apt"	
-fi
+verify_distrib() { 
+	## Set user distrib
+	echo "Verify Linux OS Distribution (Script only compatible with Debian or Redhat)"
+	if [ -f /etc/lsb-release ]; then
+		echo "Debian detected"
+		PACKAGE_MANAGER="apt"	
+	else
+		echo "Can't resolve OS distribution"
+		exit 1
+	fi
+}
 
-## All options available to user
+## All options configuration 
 case $1 in 
-	--git)
-	verify_package $1 default_install
-	;;
-	--vagrant)
-	echo "Vagrant required virtualbox in order to work"
-	verify_package --virtualbox default_install
-	verify_package $1 default_install
-	;;
-	--virtualbox)
-	verify_package $1 default_install
-	;;
+	-l|--list)
+		echo -e "Packages available:\n"
+
+		for package in ${PACKAGE_LIST[@]}; do
+			echo -e "\t$package"
+		done
+	
+		echo -e "\nIf you want to install a package please run the following lt command:" 
+		echo -e "\n\tlt --install package_name\n"
+		exit 0 
+		;;
+
+	-h|--help)
+		echo "$DISPLAY_HELP"
+		exit 0
+		;;	
+
+	-v|--version)
+		echo "v$LT_VERSION"
+		exit 0
+		;;	
+
+	-i|--install)
+		if [ -z $2 ]; then
+			echo "Please provide a package to install."
+			echo "Too see the list of available package please run: lt -l"
+			exit 1
+		else
+			verify_distrib
+
+			if [[ " ${PACKAGE_LIST[@]} " =~ " $2 " ]]; then
+				verify_package $2
+			else
+				echo "Package $2 doesn't not exists, please provide a valid package"
+				echo "Too see the list of available package please run: lt -l"
+				exit 1
+			fi
+		fi	
+		exit 0
+		;;	
 esac
